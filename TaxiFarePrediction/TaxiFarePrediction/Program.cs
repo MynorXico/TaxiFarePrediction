f4a48589c51163ac7d8981faa53a53ad0534b89e
Mynor Xico <mynorottoniel.xico2@gmail.com>
2018-06-24 19:50:13 -0600

    Create a Learning Pipeline

--- TaxiFarePrediction/TaxiFarePrediction/Program.cs
+++ TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// To create a learning pipeline
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+
 namespace TaxiFarePrediction
 {
+
     class Program
     {
+        static readonly string _datapath = Path.Combine(Environment.CurrentDirectory, "Data", "train.csv");
+        static readonly string _testdatapath = Path.Combine(Environment.CurrentDirectory, "Data", "test.csv");
+        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
         static void Main(string[] args)
         {
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = Train();
+        }
+
+        public static PredictionModel<TaxiTrip, TaxiTripFarePrediction> Train()
+        {
+            LearningPipeline pipeline = new LearningPipeline();
+
         }
     }
 }
